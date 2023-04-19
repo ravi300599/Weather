d57715cbe01f3dac7cbbc3dd449e39295822be51
ravi300599 <ravik300599@gmail.com>
2023-04-19 12:29:08 +0530

    git-weather

---
+++ src/index.js
@@ -0,0 +1,8 @@
+img = [
+    "https://25.media.tumblr.com/tumblr_m92lu1fvYU1rt04f0o1_500.gif",//snow
+    "https://thumbs.gfycat.com/RichGracefulBigmouthbass-max-1mb.gif",//Fog
+    "https://i.gifer.com/origin/09/09bcf77797226cfd979266a3b8a6fd5d_w200.webp",//thunder
+    "https://i.gifer.com/origin/fc/fcf39d8594de5764f7abc800f51a3bc0_w200.webp",//cloud
+    "https://i.gifer.com/origin/c2/c2e628069a3ee99a6676673bc2157186_w200.webp",//rain
+    "https://media.tenor.com/YzKdgLflT7MAAAAC/sunset-sunrise.gif"//clear
+];
